0ab248bacb69315b86c0c347ba265cf4bdab19e4
Erik Guillén <erik.ur.gf.10@gmail.com>
2024-06-09 12:30:45 -0500

    agregado seeder para produccion

---
+++ database/seeders/DatabaseProdSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseProdSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+            DepartamentoUbigeoSeeder::class,
+            ProvinciaUbigeoSeeder::class,
+            DistritoUbigeoSeeder::class,
+            PuntoAccesoSeeder::class,
+        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
