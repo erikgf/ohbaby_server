7a56003214b7cb3911525908fca76d5987b61085
Erik Guillén <erik.ur.gf.10@gmail.com>
2024-11-01 14:06:54 -0500

    actualizacion data octubre completo

--- database/seeders/AsistenciaRegistroEmpleadoSeeder.php
+++ database/seeders/AsistenciaRegistroEmpleadoSeeder.php
@@ -23,8 +23,9 @@ class AsistenciaRegistroEmpleadoSeeder extends Seeder
         //$this->v_20240808_01();
         //$this->v_20241002_01();
         //$this->v_20241004_01();
-        $this->v_20241031_01();
-        $this->v_20241031_02();
+        //$this->v_20241031_01();
+        //$this->v_20241031_02();
+        $this->v_20241101_01();
     }
 
     private function insertarAsistenciaMasiva(string $archivo): void{
@@ -197,6 +198,23 @@ class AsistenciaRegistroEmpleadoSeeder extends Seeder
 
         $archivo = "asistencias_masivas_v_20241031_02.csv";
         AsistenciaRegistroEmpleado::where("fecha",">=","2024-10-30")->forceDelete();
+
+        try {
+            $this->insertarAsistenciaMasiva($archivo);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            $this->command->error("ERROR {$th->getMessage()}");
+        }
+
+        DB::commit();
+    }
+
+    private function v_20241101_01(): void{
+
+        DB::beginTransaction();
+
+        $archivo = "asistencias_masivas_v_20241101_01.csv";
+        AsistenciaRegistroEmpleado::where("fecha",">=","2024-10-01")->forceDelete();
 
         try {
             $this->insertarAsistenciaMasiva($archivo);
